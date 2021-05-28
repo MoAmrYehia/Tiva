5cbca4fd4f5e3ce4ca579aaf9f8d691f4b8c8d11
tokamh <73889436+tokamh@users.noreply.github.com>
2021-05-28 22:35:25 +0200

    Update ports_init.c

--- ports_init.c
+++ ports_init.c
@@ -14,6 +14,8 @@
 #define GPIO_PORTA_DEN_R        (*((volatile unsigned long *) 0x4000451C))
 #define GPIO_PORTA_CR_R         (*((volatile unsigned long *) 0x40004524))
 #define GPIO_PORTA_AMSEL_R      (*((volatile unsigned long *) 0x40004528))
+#define GPIO_PORTA_PCTL_R       (*((volatile unsigned long *) 0x4000452C))
+#define GPIO_PORTA_LOCK_R       (*((volatile unsigned long *) 0x40004520))
 //B
 #define GPIO_PORTB_DATA_R       (*((volatile unsigned long *) 0x400053FC))
 #define GPIO_PORTB_DIR_R        (*((volatile unsigned long *) 0x40005400))
@@ -22,6 +24,8 @@
 #define GPIO_PORTB_DEN_R        (*((volatile unsigned long *) 0x4000551C))
 #define GPIO_PORTB_CR_R         (*((volatile unsigned long *) 0x40005524))
 #define GPIO_PORTB_AMSEL_R      (*((volatile unsigned long *) 0x40005528))
+#define GPIO_PORTB_LOCK_R       (*((volatile unsigned long *) 0x40005520))
+#define GPIO_PORTB_PCTL_R       (*((volatile unsigned long *) 0x4000552C))
 // //c doesn't need
 // #define GPIO_PORTC_DATA_R       (*((volatile unsigned long *) 0x400063FC))
 // #define GPIO_PORTC_DIR_R        (*((volatile unsigned long *) 0x40006400))
@@ -38,6 +42,8 @@
 #define GPIO_PORTD_DEN_R        (*((volatile unsigned long *) 0x4000751C))
 #define GPIO_PORTD_CR_R         (*((volatile unsigned long *) 0x40007524))
 #define GPIO_PORTD_AMSEL_R      (*((volatile unsigned long *) 0x40007528))
+#define GPIO_PORTD_LOCK_R       (*((volatile unsigned long *) 0x40007520))
+#define GPIO_PORTD_PCTL_R       (*((volatile unsigned long *) 0x4000752C))
 //E
 #define GPIO_PORTE_DATA_R       (*((volatile unsigned long *) 0x400243FC))
 #define GPIO_PORTE_DIR_R        (*((volatile unsigned long *) 0x40024400))
@@ -46,6 +52,8 @@
 #define GPIO_PORTE_DEN_R        (*((volatile unsigned long *) 0x4002451C))
 #define GPIO_PORTE_CR_R         (*((volatile unsigned long *) 0x40024524))
 #define GPIO_PORTE_AMSEL_R      (*((volatile unsigned long *) 0x40024528))
+#define GPIO_PORTE_LOCK_R       (*((volatile unsigned long *) 0x40024520))
+#define GPIO_PORTE_PCTL_R       (*((volatile unsigned long *) 0x4002452C))
 //F
 #define GPIO_PORTF_DATA_R       (*((volatile unsigned long *) 0x400253FC))
 #define GPIO_PORTF_DIR_R        (*((volatile unsigned long *) 0x40025400))
@@ -54,6 +62,8 @@
 #define GPIO_PORTF_DEN_R        (*((volatile unsigned long *) 0x4002551C))
 #define GPIO_PORTF_CR_R         (*((volatile unsigned long *) 0x40025524))
 #define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *) 0x40025528))
+#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *) 0x40025520))
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *) 0x4002552C))
 
 
 void portA_init(void)
