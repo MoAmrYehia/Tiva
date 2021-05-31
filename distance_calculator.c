ed5e55c9be2e77bc5decc074385f5e2e5672bbb1
Mohamed Amr <40462023+MoAmrYehia@users.noreply.github.com>
2021-05-31 17:35:10 +0200

    Update distance_calculator.c

--- distance_calculator.c
+++ distance_calculator.c
@@ -49,6 +49,11 @@ double distance(double lat1, double lon1, double lat2, double lon2) {
 double deg2rad(double deg) {
     return (deg * pi / 180);
 }
+
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::  This function converts radians to decimal degrees             :*/
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
 double rad2deg(double rad) {
     return (rad * 180 / pi);
 }
