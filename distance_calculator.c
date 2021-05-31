4fe14ebf0e6d96e4a781a37d03965515a4e4e8bc
Mohamed Amr <40462023+MoAmrYehia@users.noreply.github.com>
2021-05-31 17:34:00 +0200

    Update distance_calculator.c

--- distance_calculator.c
+++ distance_calculator.c
@@ -36,7 +36,7 @@ double distance(double lat1, double lon1, double lat2, double lon2) {
         dist = sin(deg2rad(lat1)) * sin(deg2rad(lat2)) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * cos(deg2rad(theta));
         dist = acos(dist);
         dist = rad2deg(dist);
-        dist = dist * 60 * 1.1515 * 1.609344 * 1000;
+        dist = dist * 60 * 1.1515 * 1.609344 * 1000; // This line converts distant from the degrees to metric unit
         return (dist);
     }
 }
