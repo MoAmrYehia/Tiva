e1e88a348054cf5f92bdc1452cc14ea80f68cc70
GehadHisham20 <73800429+GehadHisham20@users.noreply.github.com>
2021-06-02 17:37:06 +0200

    LCD initialization

--- display_on_lcd.c
+++ display_on_lcd.c
@@ -17,3 +17,17 @@ void Delay_Micro(unsigned long n)
 		for(i=0;i<n;i++)
 				for(j=0;j<3;j++);  
 }
+
+void LCD_INIT(void)
+{
+	SYSCTL->RCGCGPIO |=0x01;
+	SYSCTL->RCGCGPIO |=0x02;
+	GPIOA->DIR |=0xE0;  // RS, E, RW  ---- PA7, PA6, PA5
+	GPIOA->DEN |=0xE0;  
+	GPIOB->DIR |=0xFF; 
+	GPIOB->DEN |=0xFF;
+	LCD_CMD(0x38);  //8bit 2displaylines, 5*7 font
+	LCD_CMD(0x06); // increment automatically
+	LCD_CMD(0x0F);  // turn on display
+	LCD_CMD(0x01);  //clear display
+}
