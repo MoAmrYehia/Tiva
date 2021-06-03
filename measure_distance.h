e547367ea519ec6a18d4977e1fbd3497f78f02b1
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2021-06-04 00:47:33 +0200

    Update measure_distance.h

--- measure_distance.h
+++ measure_distance.h
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include "ports_init.h"
 #define PI 3.14159265358979323846 
+
 void delay_us(uint32_t delay);
 double toRadians(double degree);
 double getDistance(double lat1, double lon1, double lat2, double lon2);
