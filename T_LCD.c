a880a505bcc9e43c4619eefc4503b355e3c35c23
tokamh <73889436+tokamh@users.noreply.github.com>
2021-06-03 20:48:39 +0200

    Update T_LCD.c

--- T_LCD.c
+++ T_LCD.c
@@ -1,3 +1,6 @@
-#include "TM4C123.h" // Device header
+#include "TM4C123.h" //header
 #define LCD_DATA GPIOB
 #define LCD_CTRL GPIOA
+#define RS 0x20 /* PORTA BIT5 mask */
+#define RW 0x40 /* PORTA BIT6 mask */
+#define EN 0x80 /* PORTA BIT7 mask */
