9e000c2bcaae91167894ffb2c8ca4758b7bd1c64
tokamh <73889436+tokamh@users.noreply.github.com>
2021-06-03 21:22:52 +0200

    Update T_LCD.c

--- T_LCD.c
+++ T_LCD.c
@@ -1,9 +1,11 @@
 #include "TM4C123.h" //header
+
 #define LCD_DATA GPIOB
 #define LCD_CTRL GPIOA
 #define RS 0x20 /* PORTA BIT5 mask */
 #define RW 0x40 /* PORTA BIT6 mask */
 #define EN 0x80 /* PORTA BIT7 mask */
+
 void delayMs(int n);
 void delayUs(int n);
 void LCD_command(unsigned char command);
@@ -13,15 +15,19 @@ void keypad_init(void);
 unsigned char keypad_kbhit(void);
 void keypad_init(void);
 unsigned char keypad_getkey(void);
+
+
 int main(void)
 {
 unsigned char key;
 keypad_init();
 LCD_init();
+  
 while(1)
 {
 LCD_command(0x80);
-key = keypad_getkey(); 
+key = keypad_getkey();
+  
 if (key != 0)
 {
 LCD_data(key);
