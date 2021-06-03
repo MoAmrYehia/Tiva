006856741c47b99eae7e3335ff42cb90909fbdb0
GehadHisham20 <73800429+GehadHisham20@users.noreply.github.com>
2021-06-03 21:29:39 +0200

    Update Distance.c

--- Distance.c
+++ Distance.c
@@ -1,5 +1,6 @@
-#include "TM4C123GH6PM.h"
+#include "TM4C123GH6PM.h"  //header files for TM4C123 device and sprintf library 
 #include <stdio.h>
+//Function prototype for Timer0A and UART module initialization 
 uint32_t Measure_distance(void);
 void Timer0ACapture_init(void);
 void Delay_MicroSecond(int time);
@@ -7,19 +8,21 @@ void UART5_init(void);
 void UART5_Transmitter(unsigned char data);
 void printstring(char *str);
 void Delay(unsigned long counter);
-uint32_t time;
-uint32_t distance; 
-char mesg[20];  
+// global variables to store and display distance in m 
+uint32_t time;    //stores pulse on time 
+uint32_t distance; //stores measured distance value
+char mesg[20];     //string format of distance value
+//main code to take distance measurement and send data to UART terminal
 int main(void)
 {
-Timer0ACapture_init();  
-UART5_init();
+Timer0ACapture_init();  //initialize Timer0A in edge edge time
+UART5_init();           //initialize UART5 module to transmit data to computer
 	while(1)
 	{
-time = Measure_distance();  
-distance = (time * 10625)/10000000; 
-sprintf(mesg, "\r\nDistance = %d cm", distance); 
-printstring(mesg); 
+time = Measure_distance();    //take pulse duration measurement
+distance = (time * 10625)/10000000;      //convert pulse duration into distance
+sprintf(mesg, "\r\nDistance = %d m", distance); //convert float type distance data into string
+printstring(mesg);  //transmit data to computer
 Delay(2000);
 
 	}
