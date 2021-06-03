d0fbb4273932ab72a017e1cfac1fa9a2eeef1af7
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2021-06-04 00:49:59 +0200

    Update measure_distance.c

--- measure_distance.c
+++ measure_distance.c
@@ -25,3 +25,11 @@ void delay_us(uint32_t delay)
     TIMER0_CTL_R &=~1; // disable the timer to config it
     TIMER0_CFG_R=0;   //32-bit timer mode
     TIMER0_TAMR_R=2; //periodic mode
+     TIMER0_TAMR_R |=0x10;  // up select
+    TIMER0_TAILR_R = 80*delay-1;   // interval value to count
+    TIMER0_ICR_R |=1;  //clear the timeout flag
+    TIMER0_CTL_R |=1; // timer enable and counting starts
+    while(!TIMER0_RIS_R &1);  // wait to finish the interval
+
+
+}
