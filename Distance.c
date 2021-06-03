a639d6f8ba1654c8fd26c5a8354f264d1e560676
GehadHisham20 <73800429+GehadHisham20@users.noreply.github.com>
2021-06-03 23:34:40 +0200

    Update Distance.c

--- Distance.c
+++ Distance.c
@@ -91,7 +91,26 @@ void Delay_MicroSecond(int time)
 
     for(i = 0; i < time; i++)
     {
-        while ((TIMER1->RIS & 0x1) == 0) ;      /* wait for TimerA timeout flag 
+        while ((TIMER1->RIS & 0x1) == 0) ;      // wait for TimerA timeout flag 
         TIMER1->ICR = 0x1;      // clear the TimerA timeout flag 
     }
+}
+
+void UART5_init(void)
+{
+	  SYSCTL->RCGCUART |= 0x20;  // enable clock to UART5 
+    SYSCTL->RCGCGPIO |= 0x10;  // enable clock to PORTE for PE4/Rx and RE5/Tx 
+    // UART0 initialization 
+    UART5->CTL = 0;         // UART5 module disbable 
+    UART5->IBRD = 104;      // for 9600 baud rate, integer = 104 
+    UART5->FBRD = 11;       // for 9600 baud rate, fractional = 11
+    UART5->CC = 0;          //select system clock
+    UART5->LCRH = 0x60;     // data lenght 8-bit, not parity bit, no FIFO 
+    UART5->CTL = 0x301;     // Enable UART5 module, Rx and Tx 
+
+    // UART5 TX5 and RX5 use PE4 and PE5. Configure them digital and enable alternate function 
+    GPIOE->DEN = 0x30;      // set PE4 and PE5 as digital 
+    GPIOE->AFSEL = 0x30;    // Use PE4,PE5 alternate function 
+    GPIOE->AMSEL = 0;    // Turn off analg function
+    GPIOE->PCTL = 0x00110000;     // configure PE4 and PE5 for UART 
 }
