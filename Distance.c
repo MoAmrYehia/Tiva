a4a2c795761d1fa852d5e8d3dacc41fe25f95148
GehadHisham20 <73800429+GehadHisham20@users.noreply.github.com>
2021-06-03 23:32:15 +0200

    Update Distance.c

--- Distance.c
+++ Distance.c
@@ -76,3 +76,22 @@ void Timer0ACapture_init(void)
     TIMER0->CTL |=0x0C;        // capture the rising edge */
     TIMER0->CTL |= (1<<0);           // enable timer0A */
 }
+// Create one microsecond second delay using Timer block 1 and sub timer A 
+
+void Delay_MicroSecond(int time)
+{
+    int i;
+    SYSCTL->RCGCTIMER |= 2;     // enable clock to Timer Block 1 
+    TIMER1->CTL = 0;            // disable Timer before initialization 
+    TIMER1->CFG = 0x04;         // 16-bit option  
+    TIMER1->TAMR = 0x02;        // periodic mode and down-counter 
+    TIMER1->TAILR = 16 - 1;  // TimerA interval load value reg 
+    TIMER1->ICR = 0x1;          //clear the TimerA timeout flag 
+    TIMER1->CTL |= 0x01;        // enable Timer A after initialization 
+
+    for(i = 0; i < time; i++)
+    {
+        while ((TIMER1->RIS & 0x1) == 0) ;      /* wait for TimerA timeout flag 
+        TIMER1->ICR = 0x1;      // clear the TimerA timeout flag 
+    }
+}
