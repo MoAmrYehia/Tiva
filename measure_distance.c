74c11bf42cc0642f7f7552d035a1a6838589da13
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2021-06-04 00:48:03 +0200

    Update measure_distance.c

--- measure_distance.c
+++ measure_distance.c
@@ -2,9 +2,11 @@
  * measure_distance.c
  *
  *  Created on: May 28, 2021
- *      Author: ehab
+ *      Author: Dina
  */
  
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdint.h>
+#include "ports_init.h"
