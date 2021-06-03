80c4e6c8a2e1ec1fd708413ca979fe64a7fd6d4a
GehadHisham20 <73800429+GehadHisham20@users.noreply.github.com>
2021-06-03 20:48:01 +0200

    Update Distance_G.c

--- Distance_G.c
+++ Distance_G.c
@@ -3,3 +3,7 @@
 uint32_t Measure_distance(void);
 void Timer0ACapture_init(void);
 void Delay_MicroSecond(int time);
+void UART5_init(void);
+void UART5_Transmitter(unsigned char data);
+void printstring(char *str);
+void Delay(unsigned long counter);
