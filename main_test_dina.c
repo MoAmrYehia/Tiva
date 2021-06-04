c080647861e80311b5c28f999eb25dbfb17c30cc
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2021-06-04 18:17:07 +0200

    Update main_test_dina.c

--- main_test_dina.c
+++ main_test_dina.c
@@ -19,3 +19,16 @@
 
 char* saatAyarla(char str[]);
 void readGPSModule();
+
+int main(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+    //GPS PINLERI
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    GPIOPinConfigure(GPIO_PD6_U2RX);
+    GPIOPinConfigure(GPIO_PD7_U2TX);
+    GPIOPinTypeUART(GPIO_PORTD_BASE,GPIO_PIN_6|GPIO_PIN_7);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART2);
+    UARTConfigSetExpClk(UART2_BASE,SysCtlClockGet(),9600,(UART_CONFIG_WLEN_8 | UART_CONFIG_PAR_NONE | UART_CONFIG_STOP_ONE));
+    UARTEnable(UART2_BASE);
