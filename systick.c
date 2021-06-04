aec8958e37fcfc5b041dc2fb7cc1785e35bda6cf
Abdelrhman Hany <54750411+Abdelrhman-CaT@users.noreply.github.com>
2021-06-04 16:56:42 +0200

    systick wait

--- systick.c
+++ systick.c
@@ -8,3 +8,20 @@ void Systic_init(void)
 	NVIC_ST_CTRL_R = 0x00000005;   // ENABLE SYSTIC WITH CORE CLOCK
 	
 }
+
+void Systic_Wait(uint32_t delay)
+{
+	NVIC_ST_RELOAD_R = delay-1;  // number of count
+	NVIC_ST_CURRENT_R = 0;  // ANY WRITE TO CURRENT CLEARS IT
+	
+	while((NVIC_ST_CTRL_R &0x00010000)==0);  // wait
+}
+
+//wait 10ms
+
+void Systic_Wait_10ms(uint32_t delay)
+{
+	unsigned long i;
+	for(i=0; i<delay;i++)
+		Systic_Wait(800000);
+}
